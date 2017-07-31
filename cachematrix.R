f1decd307acfa6ac2235573cd1c73f0a4768cfad
Mike Campagna <tuc38054@temple.edu>
2017-07-31 14:44:57 -0400

    Update cachematrix.R
    Submission for assignment 2 in R Programming course.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions that create a special object that stores a matrix and caches it's inversion.
 
-## Write a short comment describing this function
+## The first function creates a special matrix, which is really a list containing a function that sets the values of the 
+## matrix, gets the values of the matrix, sets the value of the inversion, gets the value of the inversion.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        s <- NULL
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) s <<- solve
+        getsolve <- function() s
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## The next function calculates the inverse of the matrix, checking first that it has not already been calculated.
+## If so it will get the inverse from the cache and skip the computation. Otherwise, it calculates the inverse.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        s <- x$getsolve()
+        if(!is.null(s)) {
+                message("getting cached data")
+                return(s)
+        }
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setsolve(s)
+        s
 }
+
+#EXAMPLE
+m <- matrix(1:4, nrow = 2, ncol = 2)
+m
+cacheSolve(makeCacheMatrix(x = m))
